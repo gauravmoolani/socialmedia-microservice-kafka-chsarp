99ba0028106fe6a182997830441b3391dd53138c
gaurav moolani <gaurav.moolani@gmail.com>
2023-04-02 17:11:31 +0530

    initial commit

---
+++ CQRS-ES/CQRS.Core/Events/BaseEvent.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CQRS.Core.Messages;
+
+namespace CQRS.Core.Events
+{
+    public class BaseEvent : Message
+    {
+
+        public BaseEvent(string type)
+        {
+            this.Type = type;
+
+        }
+        public int Version { get; set; }
+        public string Type { get; set; }
+
+    }
+
+}
