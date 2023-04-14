4cd69c3ab88d9d6d5b514ea475d6a4c77e547717
gaurav moolani <gaurav.moolani@gmail.com>
2023-04-14 22:29:44 +0530

    added aggrigate root

--- SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Dispatchers/CommandDispatcher.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Dispatchers/CommandDispatcher.cs
@@ -8,15 +8,26 @@ namespace Post.Cmd.Infrastructure.Dispatchers
 {
     public class CommandDispatcher : ICommandDispatcher
     {
-        private readonly Dictionary<Type, Func<BaseCommand , Task> > handlers =  new();
+        private readonly Dictionary<Type, Func<BaseCommand, Task>> _handlers = new();
         public void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand
         {
-            throw new NotImplementedException();
+            if (_handlers.ContainsKey(typeof(T)))
+            {
+                throw new IndexOutOfRangeException("Handler is already registered");
+            }
+            _handlers.Add(typeof(T), x => handler((T)x));
         }
 
-        public Task SendAsync(BaseCommand command)
+        public async Task SendAsync(BaseCommand command)
         {
-            throw new NotImplementedException();
+            if (_handlers.TryGetValue(command.GetType(), out Func<BaseCommand, Task> handler))
+            {
+                await handler(command);
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(handler), "No Command Handler was registerd");
+            }
         }
     }
 }
